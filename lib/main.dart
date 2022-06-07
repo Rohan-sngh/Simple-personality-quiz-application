74aec13d64f409e5ea9200b855fd96535b06ff09
Rohan Kumar Singh <rohansingh.singh70@gmail.com>
2022-06-08 02:00:09 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+ class MyApp extends StatelessWidget{
+   widget build(BuildContext context)
+   {
+     return MaterialApp(home: Text('Hello!'),);
+   }
+
+ }
+}
