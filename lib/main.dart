e91704d4d7059c664a23136263c980bcb51f7a57
Rohan Kumar Singh <rohansingh.singh70@gmail.com>
2022-06-10 01:17:05 +0530

    Added options

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,34 @@ void main() {
 
 // ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    // ignore: avoid_print
+    print('Answer Chosen!');
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
-        body: const Text('This is my default text'),
+        body: Column(
+          children: [
+            const Text('The questions!'),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: const Text('Answer 3'),
+            ),
+          ],
+        ),
       ),
     );
   }
